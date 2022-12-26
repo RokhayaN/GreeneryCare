6fafbb68dffc820c3e68a283559c28e08a5247e0
RokhayaN <55556810+RokhayaN@users.noreply.github.com>
2022-12-26 16:05:40 -0600

    DOM manipulation
    create-select -modify and add elements to Dom

---
+++ main.js
@@ -0,0 +1,12 @@
+//Create or select
+let mainContainer = document.querySelector("#mainDiv")
+let navBar = document.createElement("nav");
+let logo = document.createElement("div");
+let menu = document.createElement("ul")
+
+//modify
+navBar.classList = "navbar";
+logo.classList = "logo";
+logo.src = "#";
+menu.classList = "menu-options";
+//add or append
