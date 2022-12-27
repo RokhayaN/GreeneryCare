f0eca066c1f7e4aeb62f7d19033526f18d3c880f
RokhayaN <55556810+RokhayaN@users.noreply.github.com>
2022-12-26 18:25:28 -0600

    created navbar
    creatting navbar js dom M

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 //Create or select
+// first navBar1 el
 let mainContainer = document.querySelector("#mainDiv")
-let navBar = document.createElement("nav");
+
+let navBar1 = document.createElement("nav");
 let logo = document.createElement("div");
-let menu = document.createElement("ul")
+let menu = document.createElement("ul");
+let search = document.createElement("li");
+let signIn = document.createElement("li");
+let cart = document.createElement("li");
+
+
+//2nd navbar el
+
 
 //modify
-navBar.classList = "navbar";
+navBar1.classList = "navbar";
+
 logo.classList = "logo";
 logo.src = "#";
-menu.classList = "menu-options";
+//logoImg.classList = "logo";
+
+menu.classList = "menu";
+
+search.innerHTML = "Search";
+search.classList = "search-plants"
+
+signIn.innerText = "signIn"
+signIn.classList = "signIn"
+
+//cart.classList = "cart"
+cart.innerText = "cart";
+cart.classList = "cart"
+
+
 //add or append
+mainContainer.append(navBar1);
+navBar1.append(logo,menu);
+menu.append(search,signIn,cart,);
