bb6e3e29261f60638596f4b4c187cad5f0a8b3f3
RokhayaN <55556810+RokhayaN@users.noreply.github.com>
2022-12-29 13:23:10 -0600

    Working on nav session
    created clickable link w a tag and attrbs

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 //Create or select
+
+
 // first navBar1 el
 let mainContainer = document.querySelector("#mainDiv")
 
 let navBar1 = document.createElement("nav");
 let logo = document.createElement("div");
 let menu = document.createElement("ul");
+let home = document.createElement("li")
 let search = document.createElement("li");
 let signIn = document.createElement("li");
 let cart = document.createElement("li");
+let linkToHome = document.createElement("a");
+let linkToform = document.createElement("a");
+let linkToCart = document.createElement("a")
 
 
 //2nd navbar el
@@ -16,24 +22,38 @@ let cart = document.createElement("li");
 //modify
 navBar1.classList = "navbar";
 
+logo.innerText = "Logo goes here"
 logo.classList = "logo";
 logo.src = "#";
 //logoImg.classList = "logo";
 
 menu.classList = "menu";
 
+//home.innerHTML = "Home";
+home.classList = "home";
+
 search.innerHTML = "Search";
 search.classList = "search-plants"
 
-signIn.innerText = "signIn"
+//signIn.innerText = "SignIn"
 signIn.classList = "signIn"
 
 //cart.classList = "cart"
-cart.innerText = "cart";
+//cart.innerText = "Cart";
 cart.classList = "cart"
+
+linkToCart.setAttribute("href", "#");
+linkToCart.innerHTML = "Cart"
+linkToHome.setAttribute("href", "#"); 
+linkToHome.innerHTML = "Home";
+linkToform.setAttribute("href", "#");
+linkToform.innerHTML = ("signIn")
 
 
 //add or append
 mainContainer.append(navBar1);
 navBar1.append(logo,menu);
-menu.append(search,signIn,cart,);
+menu.append(search,home,signIn,cart,);
+home.appendChild(linkToHome);
+signIn.appendChild(linkToform);
+cart.appendChild(linkToCart);
